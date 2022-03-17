406cda0c477f885f6c0fa929ee11b8967016a154
popovnv <nikitos.one@gmail.com>
2022-03-17 15:23:40 +0300

    Initial commit

---
+++ script.jsx
@@ -0,0 +1,20 @@
+class Tooltip extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    toogle() {
+
+    }
+    render() {
+
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <Tooltip text="The book you're reading now">React Quickly</Tooltip>
+    </div>,
+    document.getElementById('tooltip')
+)
