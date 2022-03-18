091ed34f7842aa370e94ac73188c75782eec6406
popovnv <nikitos.one@gmail.com>
2022-03-18 17:32:33 +0300

    Add props: action(click|hover), placement(top|bottom|left|right)

--- script.jsx
+++ script.jsx
@@ -2,25 +2,65 @@ class Tooltip extends React.Component {
     constructor(props) {
         super(props);
         this.toggle = this.toggle.bind(this)
+
+        let action = {};
+        if (props.action == 'click') {
+            action = {
+                'onClick': this.toggle
+            }
+        }
+        else if (props.action == 'hover') {
+            action = {
+                'onMouseEnter': this.toggle,
+                'onMouseOut': this.toggle,
+            }
+        }
+
         this.state = {
             opacity: false,
             top: 0,
-            left: 0
+            left: 0,
+            action: action
         }
     }
 
-    toggle() {
+    async toggle() {
         const tooltipNode = ReactDOM.findDOMNode(this)
-        const tooltipInner = tooltipNode.getElementsByClassName('tooltip-inner')[0]
+        const tooltipInner = tooltipNode.querySelector('.tooltip-inner')
 
-        this.setState({
+        await this.setState({
             opacity: !this.state.opacity,
         })
 
+        // Сlick actions for tooltip
+        if(this.props.action == 'click') {
+            const handleEvent = (e)=> {
+                if (this.state.opacity) {
+                    if(e.target != tooltipNode.firstChild) {
+                        this.setState({
+                            opacity: !this.state.opacity
+                        })
+                        // Destroy listener
+                        window.removeEventListener('click', handleEvent, false)
+                    }
+                }
+                else {
+                    // Destroy listener
+                    window.removeEventListener('click', handleEvent, false)
+                }
+            }
+
+            // Listen for a click outside the element tooltip
+            if (this.state.opacity) {
+                window.addEventListener('click', handleEvent, false)
+            }
+        }
+
+        // Сalculate tooltip placement
         switch (this.props.placement) {
             case 'top':
                 this.setState({
-                    top: tooltipNode.offsetTop - tooltipNode.offsetHeight,
+                    top: tooltipNode.offsetTop - tooltipNode.offsetHeight - 15,
                     left: tooltipNode.offsetLeft - (tooltipNode.offsetWidth / 2)
                 })
                 break
@@ -39,7 +79,7 @@ class Tooltip extends React.Component {
             case 'left':
                 this.setState({
                     top: tooltipNode.offsetTop - 3,
-                    left: tooltipNode.offsetLeft - tooltipInner.offsetWidth - 5
+                    left: tooltipNode.offsetLeft - tooltipInner.offsetWidth  - 5
                 })
                 break
         }
@@ -60,12 +100,12 @@ class Tooltip extends React.Component {
 
         return (
             <div style={{display: 'inline'}}>
-                <span style={{color: 'blue'}}
-                      onMouseEnter={this.toggle}
-                      onMouseOut={this.toggle}
+                <span style={{color: 'blue', whiteSpace: 'nowrap'}}
+                      {...this.state.action}
                 >
                     {this.props.children}
                 </span>
+
                 <div className={`tooltip bs-tooltip-${this.props.placement}`} style={style} role="tooltip">
                     <div className="tooltip-arrow"></div>
                     <div className="tooltip-inner">
@@ -80,11 +120,13 @@ class Tooltip extends React.Component {
 ReactDOM.render(
     <div>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-        <Tooltip text="The book you're reading now" placement="bottom"> React Quickly </Tooltip>
+        <Tooltip text="The book you're reading now" placement="top" action="click"> Top (Click) </Tooltip>
         Blanditiis dignissimos excepturi facilis hic necessitatibus provident quibusdam quod repellat tenetur totam.
+        <Tooltip text="The book you're reading now" placement="bottom" action="hover"> Bottom (Hover) </Tooltip>
         Aliquid dignissimos doloribus eos facere maxime molestiae quae sit voluptates! A adipisci at consequatur corporis
+        <Tooltip text="The book you're reading now" placement="right" action="hover"> Right (Hover) </Tooltip>
         cum cupiditate dolores est magnam minima modi molestiae molestias neque omnis possimus quam, sit, voluptatum.
-
+        <Tooltip text="The book you're reading now" placement="left" action="click"> Left (Click) </Tooltip>
     </div>,
     document.getElementById('tooltip')
 )
